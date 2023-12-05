4225b14ad93d862974c2365bd911a2e7ff6c634f
askumardev <askumar.dev@outlook.com>
2023-12-05 15:51:53 +0530

    rendering form

--- src/Form.js
+++ src/Form.js
@@ -1,36 +1,36 @@
-// import React from 'react';
+import React from 'react';
 
-// function Form() {
-//   return (
-//   	<div className="form_div">
-//       <h1>Form Page</h1>
-//       <form>
-//         <label>Enter your name:
-//           <input type="text" />
-//         </label>
-//       </form>
-//     </div>
-//   )
-// }
+function Form() {
+  return (
+    <div className="form_div">
+      <h1>Form Page</h1>
+      <form>
+        <label>Enter your name:
+          <input type="text" />
+        </label>
+      </form>
+    </div>
+  )
+}
 
-// export default Form;
+export default Form;
+//
+// import React, { Component }  from 'react';
 
-import React, { Component }  from 'react';
-
-import {useState} from 'react';
+// import {useState} from 'react';
  
-export default function  ControlledComponent()  {
-	const  [inputValue, setInputValue] =  useState('');
+// export default function  ControlledComponent()  {
+//   const  [inputValue, setInputValue] =  useState('');
 
-	const  handleChange = (event) => {
-		setInputValue(event.target.value);
-	};
+//   const  handleChange = (event) => {
+//     setInputValue(event.target.value);
+//   };
 
-return  (
-<form>
-	<label>Input Value:
-	<input  type="text"  value={inputValue} onChange={handleChange} />
-	</label>
-	<p>Input Value: {inputValue}</p>
-</form>
-)};
+// return  (
+//   <form>
+//     <label>Input Value:
+//       <input  type="text"  value={inputValue} onChange={handleChange} />
+//     </label>
+//     <p>Input Value: {inputValue}</p>
+//   </form>
+// )};
