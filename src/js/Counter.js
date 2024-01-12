4608ce688f0bf588aed5e8644b9058d3b21dfdc2
askumardev <askumar.dev@outlook.com>
2024-01-12 14:59:37 +0530

    counter js update

--- src/js/Counter.js
+++ src/js/Counter.js
@@ -3,8 +3,21 @@ import { useState } from 'react';
 
 function Counter() {
   const[count, setCount] = useState(0);
+  const [value, setValue] = useState(100);
+  const [color, setColor] = useState();
+
+  const handleClick = () => {
+    if (value === 100){
+      setValue(101);
+      setColor('red');
+    }else{
+      setValue(100);
+      setColor();
+    }
+  };
   return (
     <div>
+      <button style={{ backgroundColor: color}} onClick={handleClick}>{value}</button><br></br>
       <button onClick={() => {setCount(count+1)}}>Increment</button>
       <h1>{count}</h1>
       <button onClick={() => {setCount(count-1)}}>Decrement</button>
